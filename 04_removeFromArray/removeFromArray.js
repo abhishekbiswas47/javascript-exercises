5f7ff73351149b3e5c17c288dc8a601de5c3dd11
abhishek <abiswah.2000@gmail.com>
2023-05-20 02:21:10 +0530

    Completed `Javascript Basics` `Fundamental Part 4` assignment.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...num) {
+    let newArray = array.filter(function(item) {
+        return !num.includes(item);
+    });
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let smallerNum = 0;
+    let greaterNum = 0;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return 'ERROR';
+    }
+    if (num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    if (num1 > num2){
+        smallerNum = num2;
+        greaterNum = num1;
+    }
+    else if (num2 > num1){
+        smallerNum = num1;
+        greaterNum = num2;
+    }
+    let sum = 0;
+    for (let i = smallerNum; i<= greaterNum; i++){
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
